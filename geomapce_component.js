7773b395c38ff0189c0b46a59121551f8e4b63a3
phomble <38920208+phomble@users.noreply.github.com>
2023-06-29 16:47:29 -0700

    Update geomapce_component.js

--- geomapce_component.js
+++ geomapce_component.js
@@ -8,55 +8,56 @@
     var gMyLyr; // for sublayer
     var gMyWebmap; // needs to be global for async call to onCustomWidgetAfterUpdate()
 
+    //#viewDiv for mapview
+    
     template.innerHTML = `
         <link rel="stylesheet" href="https://js.arcgis.com/4.18/esri/themes/light/main.css">
         <style>
-        #mapview {
-            width: 100%;
-            height: 100%;
+        #mapview {   
+            padding: 0;
+            margin: 0;
+          height: 100%;
+          width: 100%;
         }
-        #timeSlider {
-            position: absolute;
-            left: 5%;
-            right: 15%;
-            bottom: 20px;
-        }
+      .esri-ui-corner .esri-component.esri-widget--panel {
+    width: 230px !important;
+};
         </style>
         <div id='mapview'></div>
-        <div id='timeSlider'></div>
+       
     `;
     
     // this function takes the passed in servicelevel and issues a definition query
     // to filter service location geometries
     //
     // A definition query filters what was first retrieved from the SPL feature service 'a17e134c51f74252bca8db3c66ef032e' 
-       function applyDefinitionQuery() {
-        var svcLyr = this.gMyWebmap.findLayerById ('541cd6166cf74e22b4857b207676db22' ); 
-        console.log( "Layer is");
-        console.log( svcLyr);
+ //      function applyDefinitionQuery() {
+  //      var svcLyr = this.gMyWebmap.findLayerById ('541cd6166cf74e22b4857b207676db22' ); 
+ //       console.log( "Layer is");
+  //      console.log( svcLyr);
 
         // make layers visible a17e134c51f74252bca8db3c66ef032e
-        svcLyr.visible = true;
+//        svcLyr.visible = true;
 
         // only execute when the sublayer is loaded. Note this is asynchronous
         // so it may be skipped over during execution and be executed after exiting this function
-        svcLyr.when(function() {
-            gMyLyr = svcLyr.findSublayerById(0);    // store in global variable
-            console.log("Sublayer loaded...");
-            console.log( "Sublayer is");
-            console.log( gMyLyr);
+//        svcLyr.when(function() {
+  //          gMyLyr = svcLyr.findSublayerById(0);    // store in global variable
+    //        console.log("Sublayer loaded...");
+     //       console.log( "Sublayer is");
+      //      console.log( gMyLyr);
 
             // force sublayer visible
-            gMyLyr.visible = true;
+     //       gMyLyr.visible = true;
 
             // run the query
-            processDefinitionQuery();
-        });
-    };
+    //        processDefinitionQuery();
+//        });
+//    };
 
     // process the definition query on the passed in SPL feature sublayer
-    function processDefinitionQuery()
-    {
+//    function processDefinitionQuery()
+//    {
         // values of passedServiceType
         // 0, 1 - no service levels. Only show service locations without a guarantee of service (GoS)
         //     Note that 0 is passed in when the widget is initialized and 1 on subsequent times
@@ -67,24 +68,24 @@
         // 6 - GoS = 5
         // 7 - GoS = 6
         // 8 (default) - return all service levels
-        if (gPassedServiceType <= 1) { // display all service locations
-            gMyLyr.definitionExpression = "1 = 1"
-        } else if (gPassedServiceType === 2) { // display GoS = 1
-            gMyLyr.definitionExpression = "NODISCONCT = '1'";
-        } else if (gPassedServiceType === 3) { // display GoS = 2
-            gMyLyr.definitionExpression = "NODISCONCT = '2'";
-        } else if (gPassedServiceType === 4) { // display GoS = 3
-            gMyLyr.definitionExpression = "NODISCONCT = '3'";
-        } else if (gPassedServiceType === 5) { // display GoS = 4
-            gMyLyr.definitionExpression = "NODISCONCT = '4'";
-        } else if (gPassedServiceType === 6) { // display GoS = 5
-            gMyLyr.definitionExpression = "NODISCONCT = '5'";
-        } else if (gPassedServiceType === 7) { // display GoS = 6
-            gMyLyr.definitionExpression = "NODISCONCT = '6'";
-        } else { // default is to only display service locations with a set GoS
-            gMyLyr.definitionExpression = "NODISCONCT IN ('1', '2', '3', '4', '5', '6')";
-        }
-    }
+ //       if (gPassedServiceType <= 1) { // display all service locations
+  //          gMyLyr.definitionExpression = "1 = 1"
+   //     } else if (gPassedServiceType === 2) { // display GoS = 1
+   //         gMyLyr.definitionExpression = "NODISCONCT = '1'";
+   //     } else if (gPassedServiceType === 3) { // display GoS = 2
+    //        gMyLyr.definitionExpression = "NODISCONCT = '2'";
+  //      } else if (gPassedServiceType === 4) { // display GoS = 3
+   //         gMyLyr.definitionExpression = "NODISCONCT = '3'";
+    //    } else if (gPassedServiceType === 5) { // display GoS = 4
+   //         gMyLyr.definitionExpression = "NODISCONCT = '4'";
+    //    } else if (gPassedServiceType === 6) { // display GoS = 5
+     //       gMyLyr.definitionExpression = "NODISCONCT = '5'";
+     //   } else if (gPassedServiceType === 7) { // display GoS = 6
+     //       gMyLyr.definitionExpression = "NODISCONCT = '6'";
+     //   } else { // default is to only display service locations with a set GoS
+    //        gMyLyr.definitionExpression = "NODISCONCT IN ('1', '2', '3', '4', '5', '6')";
+     //   }
+  //  }
 
     class Map extends HTMLElement {
         constructor() {
@@ -96,23 +97,12 @@
             let that = this;
 
             require([
-                "esri/config",
-                "esri/WebMap",
-                "esri/views/MapView",
-                "esri/widgets/BasemapToggle",
-                "esri/layers/FeatureLayer",
-                "esri/widgets/TimeSlider",
-                "esri/widgets/Expand",
-                "esri/tasks/RouteTask",
-                "esri/tasks/support/RouteParameters",
-                "esri/tasks/support/FeatureSet",
-                "esri/layers/support/Sublayer",
-                "esri/Graphic",
-                "esri/views/ui/UI",
-                "esri/views/ui/DefaultUI" 
-            ], function(esriConfig, WebMap, MapView, BasemapToggle, FeatureLayer, TimeSlider, Expand, RouteTask, RouteParameters, FeatureSet, Sublayer, Graphic) {
+                 "esri/config",
+                  "esri/Map",
+                  "esri/views/MapView",
+                  "esri/layers/FeatureLayer",
+    ], (esriConfig, Map, MapView, FeatureLayer)=> {
         //
-                
 
       esriConfig.apiKey = "AAPKbe7b507e8edb44dfb63ca6aa6d13507fYqdAozH2WMvdlQbvKEmQDcXQ13ceYpzWv9oetw1LNAP6AMHd-BU9OQRPOqNph05A";
 
@@ -120,8 +110,8 @@
         basemap: "arcgis-topographic" //Basemap styles service
       });
       const view = new MapView({
-        container: "viewDiv",
-        map: map,
+        container: "mapview",
+        map: webmap,
         center: [-118.80543,34.03000], //Longitude, latitude
         zoom: 13,
         constraints: {
@@ -144,104 +134,92 @@
          //           url: "https://route-api.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World"
          //       });
         
-                //"7aee6b93589845399d4cd7275e056cd3"
-                const webmap = new WebMap ({
-                    portalItem: {
-                        id:  "992fac5768964432b2c0b088f137cd63"
-                    }
-                });
+     //
+            
+                // SQL query array
+      const parcelLayerSQL = ["Choose a SQL where clause...", "UseType = 'Residential'",  "UseType = 'Government'", "UseType = 'Irrigated Farm'", "TaxRateArea = 10853", "TaxRateArea = 10860", "TaxRateArea = 08637", "Roll_LandValue > 1000000", "Roll_LandValue < 1000000"];
+      let whereClause = parcelLayerSQL[0];
+      // Add SQL UI
+      const select = document.createElement("select","");
+      select.setAttribute("class", "esri-widget esri-select");
+      select.setAttribute("style", "width: 200px; font-family: 'Avenir Next'; font-size: 1em");
+      parcelLayerSQL.forEach(function(query){
+        let option = document.createElement("option");
+        option.innerHTML = query;
+        option.value = query;
+        select.appendChild(option);
+      });
+      view.ui.add(select, "top-right");
+       // Listen for changes
+      select.addEventListener('change', (event) => {
+        whereClause = event.target.value;
+        queryFeatureLayer(view.extent);
+      });
+            
+  // Get query layer and set up query
+      const parcelLayer = new FeatureLayer({
+        url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/LA_County_Parcels/FeatureServer/0",
+      });
 
-                gMyWebmap = webmap;  // save to global variable
+      function queryFeatureLayer(extent) {
+        const parcelQuery = {
+         where: whereClause,  // Set by select element
+         spatialRelationship: "intersects", // Relationship operation to apply
+         geometry: extent, // Restricted to visible extent of the map
+         outFields: ["APN","UseType","TaxRateCity","Roll_LandValue"], // Attributes to return
+         returnGeometry: true
+        };
+        parcelLayer.queryFeatures(parcelQuery)
+        .then((results) => {
+          displayResults(results);
+        }).catch((error) => {
+          console.log(error.error);
+        });
+      };
 
-                const view = new MapView({
-                    container: "mapview",
-                    map: webmap
-                });
-
-                // time slider widget initialization
-                const timeSlider = new TimeSlider({
-                    container: "timeSlider",
-                    view: view
-                });
-        
-                // set on click for directions
-                view.on("click", addStop);
-        
-                function addGraphic(type, point) {
-                    var graphic = new Graphic({
-                        symbol: {
-                            type: "simple-marker",
-                            color: type === "start" ? "white" : "black",
-                            size: "8px"
-                        },
-                        geometry: point
-                    });
-
-                    view.graphics.add(graphic);
-                }
-
-                function addStop( event) { // no code here
-                    // here neither
-                    if (view.graphics.length === 0) {
-                        addGraphic("start", event.mapPoint);
-                    } else if (view.graphics.length === 1) {
-                        addGraphic("finish", event.mapPoint);
-                        getRoute();
-                    } else {
-                        view.graphics.removeAll();
-                        addGraphic("start", event.mapPoint);
-                    }
-                };
-
-                function getRoute() {
-                    // Setup the route parameters
-                    var routeParams = new RouteParameters({
-                        stops: new FeatureSet({
-                            features: view.graphics.toArray() // Pass the array of graphics
-                        }),
-                        returnDirections: true
-                    });
-
-                    // Get the route
-                    routeTask.solve(routeParams).then( showRoute);
-                }
-
-                function showRoute( data)
-                {
-                    // Display the route
-                    
-                    data.routeResults.forEach(function (result) {
-                        result.route.symbol = {
-                            type: "simple-line",
-                            color: [5, 150, 255],
-                            width: 3
-                        };
-                        view.graphics.add(result.route);
-                    });
-
-                    // Display the directions
-                    var directions = document.createElement("ol");
-                    directions.classList = "esri-widget esri-widget--panel esri-directions__scroller";
-                    directions.style.marginTop = 0;
-                    directions.style.paddingTop = "15px";
-        
-                    // Show the directions
-                    var features = data.routeResults[0].directions.features;
-                    features.forEach(function (result, i) {
-                        var direction = document.createElement("li");
-                        direction.innerHTML =
-                        result.attributes.text + " (" + result.attributes.length.toFixed(2) + " miles)";
-                        directions.appendChild(direction);
-                    });
-
-                    // Add directions to the view
-                    view.ui.empty("top-right");
-                    view.ui.add(directions, "top-right");
-                }
-
-                view.when(function () {
-                    view.popup.autoOpenEnabled = true; //disable popups
-                    gWebmapInstantiated = 1; // used in onCustomWidgetAfterUpdate
+      function displayResults(results) {
+        // Create a blue polygon
+        const symbol = {
+          type: "simple-fill",
+          color: [ 20, 130, 200, 0.5 ],
+          outline: {
+            color: "white",
+            width: .5
+          },
+        };
+        const popupTemplate = {
+          title: "Parcel {APN}",
+          content: "Type: {UseType} <br> Land value: {Roll_LandValue} <br> Tax Rate City: {TaxRateCity}"
+        };
+          
+  // Assign styles and popup to features
+        results.features.map((feature) => {
+          feature.symbol = symbol;
+          feature.popupTemplate = popupTemplate;
+          return feature;
+        });
+        // Clear display
+        view.popup.close();
+        view.graphics.removeAll();
+        // Add features to graphics layer
+        view.graphics.addMany(results.features);
+      };
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            //
         
                     // Create the basemap toggle
                     var basemapToggle = new BasemapToggle({
