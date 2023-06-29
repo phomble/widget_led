bad95b5fb945cf731eab64b2d6bec65581aabca1
phomble <38920208+phomble@users.noreply.github.com>
2023-06-29 16:59:46 -0700

    Update geomapce_component.js

--- geomapce_component.js
+++ geomapce_component.js
@@ -26,117 +26,10 @@
         <div id='mapview'></div>
        
     `;
-    
-    // this function takes the passed in servicelevel and issues a definition query
-    // to filter service location geometries
-    //
-    // A definition query filters what was first retrieved from the SPL feature service 'a17e134c51f74252bca8db3c66ef032e' 
- //      function applyDefinitionQuery() {
-  //      var svcLyr = this.gMyWebmap.findLayerById ('541cd6166cf74e22b4857b207676db22' ); 
- //       console.log( "Layer is");
-  //      console.log( svcLyr);
 
-        // make layers visible a17e134c51f74252bca8db3c66ef032e
-//        svcLyr.visible = true;
 
-        // only execute when the sublayer is loaded. Note this is asynchronous
-        // so it may be skipped over during execution and be executed after exiting this function
-//        svcLyr.when(function() {
-  //          gMyLyr = svcLyr.findSublayerById(0);    // store in global variable
-    //        console.log("Sublayer loaded...");
-     //       console.log( "Sublayer is");
-      //      console.log( gMyLyr);
-
-            // force sublayer visible
-     //       gMyLyr.visible = true;
-
-            // run the query
-    //        processDefinitionQuery();
-//        });
-//    };
-
-    // process the definition query on the passed in SPL feature sublayer
-//    function processDefinitionQuery()
-//    {
-        // values of passedServiceType
-        // 0, 1 - no service levels. Only show service locations without a guarantee of service (GoS)
-        //     Note that 0 is passed in when the widget is initialized and 1 on subsequent times
-        // 2 - return any service location with a GoS = 1
-        // 3 - GoS = 2
-        // 4 - GoS = 3
-        // 5 - GoS = 4
-        // 6 - GoS = 5
-        // 7 - GoS = 6
-        // 8 (default) - return all service levels
- //       if (gPassedServiceType <= 1) { // display all service locations
-  //          gMyLyr.definitionExpression = "1 = 1"
-   //     } else if (gPassedServiceType === 2) { // display GoS = 1
-   //         gMyLyr.definitionExpression = "NODISCONCT = '1'";
-   //     } else if (gPassedServiceType === 3) { // display GoS = 2
-    //        gMyLyr.definitionExpression = "NODISCONCT = '2'";
-  //      } else if (gPassedServiceType === 4) { // display GoS = 3
-   //         gMyLyr.definitionExpression = "NODISCONCT = '3'";
-    //    } else if (gPassedServiceType === 5) { // display GoS = 4
-   //         gMyLyr.definitionExpression = "NODISCONCT = '4'";
-    //    } else if (gPassedServiceType === 6) { // display GoS = 5
-     //       gMyLyr.definitionExpression = "NODISCONCT = '5'";
-     //   } else if (gPassedServiceType === 7) { // display GoS = 6
-     //       gMyLyr.definitionExpression = "NODISCONCT = '6'";
-     //   } else { // default is to only display service locations with a set GoS
-    //        gMyLyr.definitionExpression = "NODISCONCT IN ('1', '2', '3', '4', '5', '6')";
-     //   }
-  //  }
-
-    class Map extends HTMLElement {
-        constructor() {
-            super();
-            
-            //this._shadowRoot = this.attachShadow({mode: "open"});
-            this.appendChild(template.content.cloneNode(true));
-            this._props = {};
-            let that = this;
-
-            require([
-                 "esri/config",
-                  "esri/Map",
-                  "esri/views/MapView",
-                  "esri/layers/FeatureLayer",
-    ], (esriConfig, Map, MapView, FeatureLayer)=> {
-        //
-
-      esriConfig.apiKey = "AAPKbe7b507e8edb44dfb63ca6aa6d13507fYqdAozH2WMvdlQbvKEmQDcXQ13ceYpzWv9oetw1LNAP6AMHd-BU9OQRPOqNph05A";
-
-      const map = new Map({
-        basemap: "arcgis-topographic" //Basemap styles service
-      });
-      const view = new MapView({
-        container: "mapview",
-        map: webmap,
-        center: [-118.80543,34.03000], //Longitude, latitude
-        zoom: 13,
-        constraints: {
-          snapToZoom: false
-        }                
-                
-                
-         //
-                // set portal and API Key
-        //        esriConfig.portalUrl = gPassedPortalURL
-
-                //  set esri api Key 
-        //        esriConfig.apiKey = gPassedAPIkey
-                
-                 //  set hstoken 
-           //     esriConfig.hstoken = gPassedHSToken
-        
-                // set routing service
-         //       var routeTask = new RouteTask({
-         //           url: "https://route-api.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World"
-         //       });
-        
-     //
-            
-                // SQL query array
+//filter
+// SQL query array
       const parcelLayerSQL = ["Choose a SQL where clause...", "UseType = 'Residential'",  "UseType = 'Government'", "UseType = 'Irrigated Farm'", "TaxRateArea = 10853", "TaxRateArea = 10860", "TaxRateArea = 08637", "Roll_LandValue > 1000000", "Roll_LandValue < 1000000"];
       let whereClause = parcelLayerSQL[0];
       // Add SQL UI
@@ -155,8 +48,8 @@
         whereClause = event.target.value;
         queryFeatureLayer(view.extent);
       });
-            
-  // Get query layer and set up query
+
+      // Get query layer and set up query
       const parcelLayer = new FeatureLayer({
         url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/LA_County_Parcels/FeatureServer/0",
       });
@@ -191,8 +84,7 @@
           title: "Parcel {APN}",
           content: "Type: {UseType} <br> Land value: {Roll_LandValue} <br> Tax Rate City: {TaxRateCity}"
         };
-          
-  // Assign styles and popup to features
+        // Assign styles and popup to features
         results.features.map((feature) => {
           feature.symbol = symbol;
           feature.popupTemplate = popupTemplate;
@@ -204,42 +96,45 @@
         // Add features to graphics layer
         view.graphics.addMany(results.features);
       };
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            //
-        
-                    // Create the basemap toggle
-                    var basemapToggle = new BasemapToggle({
-                        view:view,
-                        nextBasemap: "satellite"
-                    });
+    });
 
-        
-                    // Add the toggle to the bottom-right of the view
-                    view.ui.add( basemapToggle, "bottom-right");
-        
-                    // should have been set in onCustomWidgetBeforeUpdate()
-                    console.log( gPassedServiceType);
+///filter    
+    
 
-                    // find the SPL sublayer so a query is issued
-                    applyDefinitionQuery();
-                });
+    class Map extends HTMLElement {
+        constructor() {
+            super();
+            
+            //this._shadowRoot = this.attachShadow({mode: "open"});
+            this.appendChild(template.content.cloneNode(true));
+            this._props = {};
+            let that = this;
 
-            }); // end of require()
-        } // end of constructor()    
+            require([
+                 "esri/config",
+                  "esri/Map",
+                  "esri/views/MapView",
+                  "esri/layers/FeatureLayer",
+    ], (esriConfig, Map, MapView, FeatureLayer)=> {
+        //
+
+      esriConfig.apiKey = "AAPKbe7b507e8edb44dfb63ca6aa6d13507fYqdAozH2WMvdlQbvKEmQDcXQ13ceYpzWv9oetw1LNAP6AMHd-BU9OQRPOqNph05A";
+
+      const map = new Map({
+        basemap: "arcgis-topographic" //Basemap styles service
+      });
+      const view = new MapView({
+        container: "mapview",
+        map: webmap,
+        center: [-118.80543,34.03000], //Longitude, latitude
+        zoom: 13,
+        constraints: {
+          snapToZoom: false
+        }                
+     });
+ 
+            
+
 
         getSelection() {
             return this._currentSelection;
